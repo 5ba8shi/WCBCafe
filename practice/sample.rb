899f083d78e407909d4083f1b94f23f103429cca
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-01-03 12:18:32 +0900

    up

--- practice/sample.rb
+++ practice/sample.rb
@@ -1,12 +1,16 @@
-respond_to do |format|
-  format.json {
-    render json: { id: @user.id, name: @user.name }
-  }
+class TodosController < ApplicationController
+  def index
+    @todo = Todo.new
+    @todos = Todo.order('cereated_at ASC')
+  end
 
-def create
-  @todo = Todo.create(todo_params)
-  respond_to do |format|
-    format.html { redirect_to :root}
-    format.json { render json: @todo}
+  def create
+    Todo.create(todo_params)
+    redirect_to :root
+  end
+
+  private
+  def todo_params
+    params.require(:todo).permit(:content)
   end
 end

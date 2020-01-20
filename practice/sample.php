ed2d377b2e8a428ea4bda6f8471fa731a58c0390
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-01-20 15:58:46 +0900

    up

---
+++ practice/sample.php
@@ -0,0 +1,19 @@
+<?php echo'<h2>初級</h2>'; ?>
+
+<?php
+  echo'こんにちは';
+  echo'PHPを学ぼう';
+  ?>
+
+<?php
+  echo 'Hello, World!';
+  ?>
+
+<?php echo "Hello, World"; ?>
+
+<?php echo'Hello, PHP!'; ?>
+
+<?php
+echo 7 + 3;
+echo "10 - 4";
+?>

d471d03c0d6a5adddfd27c630e8d3e5f38c688b6
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-01-22 11:06:34 +0900

    up

--- practice/sample2.rb
+++ practice/sample2.rb
@@ -10,9 +10,25 @@ class SignupsController < ApplicationController
     
     @user = if session[:password_confirmation]
         User.new(
-          User.new(
-            nick
-          )
+            nickname: session[:nickname],
+            email: session[:email],
+            password_confirmation: session[:password_confirmation]
         )
+    else
+      User.new
     end
 end
+
+
+def step1_validates
+
+  @user = 
+      if session[:password_confirmation].present?
+        set_user_when_sns(user_params)
+      else
+        set_user_when_email(user_params)
+      end
+  @user.valid?
+  skip_phonenumber_validate(@user.errors)
+
+  if verify_recaptcha(model: @user)

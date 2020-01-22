a867b794b0a4de67b0ef53fd5b658f7092d56f3f
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-01-22 10:51:45 +0900

    up

--- practice/sample2.rb
+++ practice/sample2.rb
@@ -1,43 +1,18 @@
-class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  def facebook
-    callback_for(:facebook)
+class SignupsController < ApplicationController
+  before_action :authenticat_user!, only: :done
+
+  def indx
+    delete_session
+    redirect_to root_path if user_signed_in?
   end
 
-  def google_oauthn
-    callback_for(:google)
-  end
-
-  def callback_for(provider)
-    info = User.find_oauth(request.env["omniauth.auth"])
-    @user = User.where(nickname: info[:user][:nickname]).or(User.where(email: info[:user][:email])).first || info[:user]
-
-    if @user.persisted?
-
-      sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
-    else
-
-      session[:nickname] = info[:user][:nickname]
-      session[:email] = info[:user][:email]
-
-      session[:password_confirmation] = SecureRandom.alphanumeric(30)
-
-
-      if SnsCredential.find_by(uid: info[:sns][:uid], provider: info[:sns][:provider]).nil?
-
-        session[:uid] = info[:sns][:uid]
-        session[:provider] = info[:sns][:provider]
-      end
-
-      redirect_to step1_signups_path
+  def step1
+    
+    @user = if session[:password_confirmation]
+        User.new(
+          User.new(
+            nick
+          )
+        )
     end
-  end
-
-  def fialure
-    redirect_to root_path
-  end
-end
-
-
-
 end

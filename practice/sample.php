c911eb2ba59bd7ea82d64b0fa330439d9da34a84
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-01-24 12:59:54 +0900

    う@

--- practice/sample.php
+++ practice/sample.php
@@ -343,3 +343,17 @@ foreach($data as $value){
   }
 ?>
 
+
+<?php
+  for
+?>
+
+
+<?php
+  $scores = array('数学' => 70,'英語' => 90,'国語' => 80);
+
+  foreach ($scores as $key => $value) {
+    echo $key.'は'.$value.'点です。';
+  }
+
+?>

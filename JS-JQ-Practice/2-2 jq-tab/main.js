ee4f0f0f07a77e1fa336dacc44267a04024fca04
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-02-01 22:28:36 +0900

    u@

--- JS-JQ-Practice/2-2 jq-tab/main.js
+++ JS-JQ-Practice/2-2 jq-tab/main.js
@@ -1,14 +1,8 @@
-$(function(){
+$(function() {
   let tabs = $(".menu_item");
 
   function tabSwitch() {
-    $('.active').removeClass("active");
-  
-    $(this).addClass("active");
 
-    const index = tabs.index(this);
-
-    $(".content").removeClass("show").eq(index).addClass("show");
   }
   tabs.click(tabSwitch);
 });

e0e07de6a723f6410812a6cf82d2839f1214fe75
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-02-02 06:54:02 +0900

    u

--- JS-JQ-Practice/4 jquery-exercise/main.js
+++ JS-JQ-Practice/4 jquery-exercise/main.js
@@ -4,6 +4,30 @@ $(function(){
   let list = $("#list");
 
   function appendList(word){
-    let item = $('<li class="list">').append
+    let item = $('<li class="list">').append(word);
+    list.append(item);
   }
-})
+
+  function editElement(element){
+    let result = "^" + element;
+    return result;
+  }
+
+  $("#submit").on("click", function(){
+    let input = $("keyword").val();
+    let inputs = input.split(" ");
+    let newInputs = inputs.map(editElement);
+    let reg = RegExp(newInputs.join("|"));
+
+    $(".list").remove();
+
+    $.each(fruits, function(i, fruit){
+      if(fruit.match(reg)) {
+        appendList(fruit);
+      }
+    });
+    if($(".list").length === 0){
+      appendList("一致する果物はありませんでした");
+    }
+  });
+});

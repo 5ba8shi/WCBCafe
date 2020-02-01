8697eeee275448debb7cddaa4ed761ba98de5c3c
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-02-01 22:44:25 +0900

    up

--- JS-JQ-Practice/2-2 jq-tab/main.js
+++ JS-JQ-Practice/2-2 jq-tab/main.js
@@ -2,7 +2,13 @@ $(function() {
   let tabs = $(".menu_item");
 
   function tabSwitch() {
+    $('.active').removeClass("active");
 
+    $(this).addClass("active");
+
+    const index = tabs.index(this);
+
+    $(".content").removeClass("show").eq(index).addClass("show");
   }
   tabs.click(tabSwitch);
 });

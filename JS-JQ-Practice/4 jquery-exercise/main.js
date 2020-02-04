e4ccb3a599ffe69c714fd6d9bda0e09d6f6efb28
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-02-04 19:17:30 +0900

    Update main.js

--- JS-JQ-Practice/4 jquery-exercise/main.js
+++ JS-JQ-Practice/4 jquery-exercise/main.js
@@ -1,34 +1,24 @@
 let fruits = ['apple', 'apricot', 'avocado', 'blueberry', 'cherry', 'coconut', 'cranberry', 'dragonfruit', 'durian', 'grape', 'grapefruit', 'guava', 'kiwi fruit', 'lemon', 'lime', 'lychee', 'mango', 'melon', 'watermelon', 'miracle fruit', 'orange', 'bloodorange','clementine','mandarine','tangerine','papaya','passionfruit','peach','pear','persimmon','physalis','plum/prune','pineapple','pomegranate','raspberry','rambutan','star fruit','strawberry'];
 
 $(function(){
-  let = $("#list");
-
-  function appendList(word) {
-    let item = $('<li class="list">').append(word);
-    list.append(item);
-  }
+  let list = $("#list");
 
   function editElement(element){
     let result = "^" + element;
     return result;
-  }
+  } 
 
   $("#submit").on("click", function(){
     let input = $("#keyword").val();
     let inputs = input.split(" ");
-    let newInputs = inputs.map(editElement); 
+    let newInputs = inputs.map(editElement);
     let reg = RegExp(newInputs.join("|"));
 
-    $(".list").remove();
 
-    $.each(fruits, function(i, fruit){
-      if(fruit.match(reg)) {
+    $.each(fruits, function(i, fruit) {
+      if (fruit.match(reg)){
         appendList(fruit);
       }
-    });
-
-    if ($(".list").length === 0) {
-      appendList("一致する果物はありませんでした");
-    }
+    })
   });
 });

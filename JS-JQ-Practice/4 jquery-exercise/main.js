9299e50f6175fe776db74f9c39f416e7e113731a
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-02-04 19:10:40 +0900

    Update main.js

--- JS-JQ-Practice/4 jquery-exercise/main.js
+++ JS-JQ-Practice/4 jquery-exercise/main.js
@@ -1,13 +1,13 @@
 let fruits = ['apple', 'apricot', 'avocado', 'blueberry', 'cherry', 'coconut', 'cranberry', 'dragonfruit', 'durian', 'grape', 'grapefruit', 'guava', 'kiwi fruit', 'lemon', 'lime', 'lychee', 'mango', 'melon', 'watermelon', 'miracle fruit', 'orange', 'bloodorange','clementine','mandarine','tangerine','papaya','passionfruit','peach','pear','persimmon','physalis','plum/prune','pineapple','pomegranate','raspberry','rambutan','star fruit','strawberry'];
 
 $(function(){
-  let list = $("#list");
+  let = $("#list");
 
   function appendList(word) {
     let item = $('<li class="list">').append(word);
     list.append(item);
   }
-  
+
   function editElement(element){
     let result = "^" + element;
     return result;
@@ -16,13 +16,13 @@ $(function(){
   $("#submit").on("click", function(){
     let input = $("#keyword").val();
     let inputs = input.split(" ");
-    let newInputs = inputs.map(editElement);
+    let newInputs = inputs.map(editElement); 
     let reg = RegExp(newInputs.join("|"));
 
     $(".list").remove();
 
-    $.each(fruits, function(i, fruit) {
-      if (fruit.match(reg)) {
+    $.each(fruits, function(i, fruit){
+      if(fruit.match(reg)) {
         appendList(fruit);
       }
     });

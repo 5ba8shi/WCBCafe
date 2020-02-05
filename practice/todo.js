bc147de9e91746b2bbd27448ab41478d9c52890c
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-02-06 04:36:56 +0900

    Update todo.js

--- practice/todo.js
+++ practice/todo.js
@@ -191,3 +191,19 @@ $(function() {
     alert('あなたが選んだ果物:\n' + output);
   });
 });
+
+
+$(function() {
+  let tabs = $(".menu_item");
+
+  function tabSwitch() {
+    $('.active').removeClass("active");
+
+    $(this).addClass("active");
+
+    const index = tabs.index(this);
+
+    $(".content").removeClass("show").eq(index).addClass("show");
+  }
+  tabs.click(tabSwitch);
+});

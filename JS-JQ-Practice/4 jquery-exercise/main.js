0ebe21ccfde49daaf760eb54172adb6dbf21f975
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-02-05 16:53:28 +0900

    Update main.js

--- JS-JQ-Practice/4 jquery-exercise/main.js
+++ JS-JQ-Practice/4 jquery-exercise/main.js
@@ -4,16 +4,17 @@ $(function(){
   let list = $("#list");
 
   function appendList(word) {
-    let item = $('<li class = "list">').append(word);
+    let item = $('<li class="list">').append(word);
     list.append(item);
   }
 
-  function editElement(element){
+  function editElement(element) {
     let result = "^" + element;
     return result;
   }
-  $("#submit").on("click", function(){
-    let input = $("#keyword").val();
+
+  $("#submit").on("click", function() {
+    let input = $("keyword").val();
     let inputs = input.split(" ");
     let newInputs = inputs.map(editElement);
     let reg = RegExp(newInputs.join("|"));
@@ -21,7 +22,7 @@ $(function(){
     $(".list").remove();
 
     $.each(fruits, function(i, fruit){
-      if(fruit.match(reg)) {
+      if (fruit.match(reg)) {
         appendList(fruit);
       }
     });

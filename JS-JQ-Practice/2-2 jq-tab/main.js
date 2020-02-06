dcea65ab8cd1d929482fde6287e47eba691b67e1
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-02-07 07:56:19 +0900

    Update main.js

--- JS-JQ-Practice/2-2 jq-tab/main.js
+++ JS-JQ-Practice/2-2 jq-tab/main.js
@@ -1,14 +1,14 @@
 $(function(){
   let tabs = $(".menu_item");
-
+  
   function tabSwitch(){
-    $('.active').removeClass("active");
+    $('active').removeClass("active");
 
     $(this).addClass("active");
 
     const index = tabs.index(this);
 
-    $(".content").removeClass("show").eq(this).addClass("show");
+    $(".content").removeClass("show").eq(index).addClass("show");
   }
   tabs.click(tabSwitch);
 });

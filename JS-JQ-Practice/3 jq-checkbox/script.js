0e086744fa8f0ffddc3ce66296cef371edc3c436
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-02-07 15:00:36 +0900

    Update script.js

--- JS-JQ-Practice/3 jq-checkbox/script.js
+++ JS-JQ-Practice/3 jq-checkbox/script.js
@@ -1,14 +1,10 @@
-$(function(){
-  $('form').on('submit', function(e){
+$(function(e){
+  $("form").on(function(e){
     let output = '';
-    let checkboxes = $(this).find('input[type="checkbox"]');
-    checkboxes.each(function(i, checkbox){
-      checkbox = $(checkboxes[i]);
-      if (checkbox.prop('checked')) {
-        output += checkbox.attr('value') + '\n';
-      }
-    });
+    let checkboxes = $(this).find
+    
     e.preventDefault();
-    alert('あなたが選んだ果物:\n' + output);
+    alert('あなたが選んだ果物は:\n' + output);
   });
+
 });

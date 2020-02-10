6b0d6821a92b49f27e2d92bd75ce032e0b547a81
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-02-11 06:40:09 +0900

    Update main.js

---
+++ JS-JQ-Practice/2-1 js-tab/main.js
@@ -0,0 +1,10 @@
+window.addEventListener("load", function(){
+  let tabs = document.getElementsByClassName("menu_item");
+
+  tabsAry = Array.prototype.slice.call(tabs);
+
+  function tabSwitch() {
+  }
+  tabsAry.forEach(function(value){
+  });
+});

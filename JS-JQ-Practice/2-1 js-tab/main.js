e2fe4818720d53481f0982afa95f3300afb16dc0
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-02-15 13:19:29 +0900

    e

--- JS-JQ-Practice/2-1 js-tab/main.js
+++ JS-JQ-Practice/2-1 js-tab/main.js
@@ -5,16 +5,9 @@ window.addEventListener("load", function(){
 
   function tabSwitch(){
     document.getElementsByClassName("active")[0].classList.remove("active");
-
-    this.classList.add("active");
-
-    document.getElementsByClassName("show")[0].classList.remove("show");
-
-    const index = tabsAry.indexOf(this);
-
-    document.getElementsByClassName("content")[index].classList.add("show");
+    
   }
-  tabsAry.forEach(function(value){
+  tabsAry.forEach(function(value) {
     value.addEventListener("click", tabSwitch);
   });
 });

c1f61fdc3e3f00afbac73d18cc635613ba0e08cb
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-02-15 15:42:41 +0900

    up

--- practice/todo.js
+++ practice/todo.js
@@ -1,3 +1,25 @@
+//PAIZA
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+var lines = []
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+reader.on('line', (line) => {
+  lines.push(line);
+});
+
+reader.on('close', () => {
+  var N = lines[0];
+  for(var i = 0; i < N; i++) {
+    var line = lines[i+1].split(" ");
+    console.log("hello = " + line[0] + ", world = " + line[1]);
+  }
+});
+
 $(function() {
 
   var search_list = $(".contents.row");

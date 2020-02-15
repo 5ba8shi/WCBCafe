03a6e547ecea8d00826b07416725e5439f803083
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-02-15 13:27:30 +0900

    Update main.js

--- JS-JQ-Practice/2-1 js-tab/main.js
+++ JS-JQ-Practice/2-1 js-tab/main.js
@@ -5,7 +5,14 @@ window.addEventListener("load", function(){
 
   function tabSwitch(){
     document.getElementsByClassName("active")[0].classList.remove("active");
-    
+
+    this.classList.add("active");
+
+    document.getElementsByClassName("show")[0].classList.remove("show");
+
+    const index = tabsAry.indexOf(this);
+
+    document.getElementsByClassName("content")[index].classList.add("show");
   }
   tabsAry.forEach(function(value) {
     value.addEventListener("click", tabSwitch);

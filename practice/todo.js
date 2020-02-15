5c3261c726177035ae3eabe35fbbb9a8628c253a
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-02-15 19:13:59 +0900

    Update todo.js

--- practice/todo.js
+++ practice/todo.js
@@ -261,3 +261,19 @@ window.addEventListener("load", function() {
     value.addEventListener("click", tabSwitch);
   });
 });
+
+
+$(function(){
+  let tabs = $(".menu_item");
+
+  function tabSwitch(){
+    $('.active').removeClass("active");
+
+    $(this).addClass("active");
+
+    const index = tabs.index(this);
+
+    $(".content").removeClass("show").eq(index).addClass("show");
+  }
+  tabs.click(tabSwitch);
+});

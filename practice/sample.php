93ded6a618c8498abe3f1ed2eceb01782712e2b5
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-02-15 16:34:41 +0900

    Update sample.php

--- practice/sample.php
+++ practice/sample.php
@@ -1,3 +1,15 @@
+<!-- PAIZA -->
+<?php
+$input_line = trim(fgets(STDIN));
+for ($i = 0; $i < $input_line; $i++) {
+  $s = trim(fgets(STDIN));
+  $s = str_replace(array("\r\n","\r","\n"), '', $s);
+  $s = explode(" ", $s);
+  echo "hello = ".$s[0]." , world = ".$s[1]."\n";
+}
+?>
+
+
 <?php echo'<h2>初級</h2>'; ?>
 
 <?php

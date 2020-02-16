f7ddb07c76574cb03ad594e022a50b5837f768a2
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-02-16 16:44:22 +0900

    Update app.js

--- PWA sample/app.js
+++ PWA sample/app.js
@@ -5,35 +5,4 @@ const startBtn = document.getElementById('js-startBtn')
 const countText = document.getElementById('js-count')
 const timeText = document.getElementById('js-time')
 
-const setGame = () => {
-  tapCount = 0
-  time = 10000
-  countText.innerText = tapCount
-  timeText.innerHTML = time / 1000
-}
-setGame()
-
-tapBtn.addEventListener('click',() =>{
-  if (!isPlaying) return false
-  tapCount++
-  countText.innerText = tapCount
-})
-
-startBtn.addEventListener('click', () =>{
-  setGame()
-  isPlaying = true
-  tapBtn.disabled = false
-  startBtn.style.display = 'none'
-
-  const timer = setInterval( ()=> {
-    time -= 10
-    timeText.innerHTML = (time / 1000) .toFixed(2)
-
-    if(time === 0) {
-      clearInterval(timer)
-      isPlaying = false
-      startBtn.style.display = 'inline-block'
-      startBtn.innerText = 'もう一回'
-    }
-  }, 10)
-})
+const setGame = () =>

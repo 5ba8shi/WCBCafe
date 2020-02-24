61c5aeb3ec18aff2322269cf498ff934ffb1d379
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-02-24 22:14:22 +0900

    Update todo.js

--- practice/todo.js
+++ practice/todo.js
@@ -516,3 +516,11 @@ window.addEventListener("load", function(){
 
   btn.addEventListener("click", printHello);
 });
+
+window.addEventListener("load", function(){
+  let btn = document.querySelector("button");
+
+  btn.addEventListener("click", function(){
+    console.log("Hello world");
+  });
+});

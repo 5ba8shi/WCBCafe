971aa708b928966d78cc39a6c30b63318f4125ae
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-03-04 21:03:58 +0900

    ｗ

--- practice/todo.js
+++ practice/todo.js
@@ -540,8 +540,12 @@ window.addEventListener("load", function(){
     document.getElementsByClassName("active")[0].classList.remove("active");
 
     this.classList.add("active");
-    
 
+    document.getElementsByClassName("show")[0].classList.remove("show");
+
+    const index = tabsAry.indexOf(this);
+
+    document.getElementsByClassName("content")[index].classList.add("show");
   }
   tabsAry.forEach(function(value){
     value.addEventListener("click", tabSwitch);

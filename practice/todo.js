63d3f9b8bf0d0472b9d7974bec3e0c1e59d8ef16
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-03-04 19:20:43 +0900

    y

--- practice/todo.js
+++ practice/todo.js
@@ -530,3 +530,20 @@ document.getElementById();
 document.getElementsByClassName
 
 document.querySelector("セレクタ名");
+
+window.addEventListener("load", function(){
+  let tabs = document.getElementsByClassName("menu_item")
+
+  tabsAry = Array.prototype.slice.call(tabs);
+
+  function tabSwitch(){
+    document.getElementsByClassName("active")[0].classList.remove("active");
+
+    this.classList.add("active");
+    
+
+  }
+  tabsAry.forEach(function(value){
+    value.addEventListener("click", tabSwitch);
+  });
+});

3b3206124fe6b61e1f6b310f0d7ca93951249ce9
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-03-12 13:19:51 +0900

    Update todo.js

--- practice/todo.js
+++ practice/todo.js
@@ -690,3 +690,24 @@ window.addEventListener("load", function(){
   });
 });
 
+window.addEventListener("load", function(){
+  let tabs = document.getElementsByClassName("menu_item");
+
+  let tabsAry = Array.prototype.slice.call(tabs);
+
+  function tabSwitch(){
+    document.getElementsByClassName("active")[0].classList.remove("active");
+
+    this.classList.add("active");
+
+    document.getElementsByClassName("show")[0].classList.remove("show");
+
+    const index = tabsAry.indexOf(this);
+
+    document.getElementsByClassName("content")[index].classList.add("show");
+
+  }
+  tabsAry.forEach(function(value){
+    value.addEventListener("click", tabSwitch)
+  });
+});

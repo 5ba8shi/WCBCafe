63a061ddb9706106fa1b2a71dfc668a122e76c5b
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-03-14 19:46:01 +0900

    Update todo.js

--- practice/todo.js
+++ practice/todo.js
@@ -689,6 +689,8 @@ window.addEventListener("load", function(){
     value.addEventListener("click", tabSwitch);
   });
 });
+
+
 window.addEventListener("load", function(){
   let tabs = document.getElementsByClassName("menu_item");
 
@@ -704,27 +706,6 @@ window.addEventListener("load", function(){
     const index = tabsAry.indexOf(this);
 
     document.getElementsByClassName("content")[index].classList.add("show");
-  }
-  tabsAry.function(function(value){
-    value.addEventListener("click", tabSwitch);
-  });
-});
-
-window.addEventListener("load", function(){
-  let tabs = document.getElementsByClassName("menu_item");
-
-  tabsAry = Array.prototype,slice.call(tabs);
-
-  function tabSwitch(){
-    document.getElementsByClassName("active")[0].classList.remove("active");
-
-    this.classList.add("active");
-
-    document.getElementsByClassName("show")[0].classList.remove("show");
-
-    const index = tabsAry.indexOf(this);
-
-    document.getElementsByClassName("content")[this].classList.add("show");
   }
 
   tabsAry.forEach(function(value){

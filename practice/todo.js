1e9cce40cd91aa7a1e9ef353651059fbd2d029c9
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-03-20 15:28:17 +0900

    Update todo.js

--- practice/todo.js
+++ practice/todo.js
@@ -951,3 +951,33 @@ btn4.addEventListener("click", function(){
 });
 
 
+$(function(){
+  let list = $("#list");
+
+  function appendList(word){
+    let item = $('<li class="list">').append(word);
+    list.append(item);
+  }
+
+  function editElement(element){
+    let result = "^" + element;
+    return result;
+  }
+
+  $('#submit').on("click", function(){
+    let input = $("#keyword").val();
+    let inputs = input.split(" ");
+    let newInput = inputs.map(editElement);
+    let reg = RegExp(newInput.join("|"));
+
+    $.each(fruits, function(i, fruit){
+      if (fruit.math(reg)){
+        appendList(fruit);
+      }
+    });
+
+    if ($(".list").length === 0){
+      appendList("一致する果物はありませんでした。")
+    }
+  });
+});

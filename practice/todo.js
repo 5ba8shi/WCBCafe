f40a4130dddd843b63f13e5edea429e30f4567d9
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-03-29 05:26:07 +0900

    Update todo.js

--- practice/todo.js
+++ practice/todo.js
@@ -1542,3 +1542,28 @@ class App extends React.Component {
     )
   }
 }
+
+
+const images = ['images/image1.jpg', 'images/image2.jpg', 'images/image3.jpg', 'images/image4.jpg', 'images/image5.jpg'];
+let current = 0;
+
+function changeImage(num){
+  if(current + num >= 0 && current + num < images.length) {
+    current += num;
+    document.getElementById('main_image').src = images[current];
+  }
+};
+
+function pageNum(){
+  document.getElementById('page').textContent = `${current + 1}/${images.length}`;
+};
+
+pageNum();
+
+document.getElementsById('prev').textContent = function(){
+  changeImage(-1);
+}
+
+document.getElementById('next').textContent = function(){
+  changeImage(1);
+}

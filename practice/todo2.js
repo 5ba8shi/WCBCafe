584d0cdb0c2667c132f462d3d4022fdb8193f426
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-04-07 20:34:55 +0900

    Update todo2.js

--- practice/todo2.js
+++ practice/todo2.js
@@ -638,3 +638,82 @@ $(function(){
 });
 
 
+let fruits = ['apple', 'apricot', 'avocado', 'blueberry', 'cherry', 'coconut', 'cranberry', 'dragonfruit', 'durian', 'grape', 'grapefruit', 'guava', 'kiwi fruit', 'lemon', 'lime', 'lychee', 'mango', 'melon', 'watermelon', 'miracle fruit', 'orange', 'bloodorange','clementine','mandarine','tangerine','papaya','passionfruit','peach','pear','persimmon','physalis','plum/prune','pineapple','pomegranate','raspberry','rambutan','star fruit','strawberry'];
+
+$(function(){
+  let list = $("#list");
+
+  function appendList(word){
+    let item = $('<li class="list">').append(word);
+    list.append(item);
+  }
+
+  function editElement(element){
+   let result = "^" + element;
+   return result;
+  }
+
+  $('#submit').on("click", function(){
+    let input = $("keyword").val();
+    let inputs = input.split(" ");
+    let newInput = inputs.map(editElement);
+    let reg = RegExp(newInput.join("|"));
+
+    $.each(fruits, function(i, fruit){
+      if(fruit.match(reg)){
+        appendList(fruit);
+      }
+    });
+
+    if($(".list").length === 0) {
+      appendList("一致する果物はありませんでした。")
+    }
+  });
+});
+
+
+$(function(){
+  $("form").on("submit", function(){
+    console.log("送信ボタンが押されました");
+  });
+});
+
+$(function(){
+  $("form").on("submit", function(){
+    console.log("送信ボタンが押されました");
+  });
+});
+
+
+let fruits = ['apple', 'apricot', 'avocado', 'blueberry', 'cherry', 'coconut', 'cranberry', 'dragonfruit', 'durian', 'grape', 'grapefruit', 'guava', 'kiwi fruit', 'lemon', 'lime', 'lychee', 'mango', 'melon', 'watermelon', 'miracle fruit', 'orange', 'bloodorange','clementine','mandarine','tangerine','papaya','passionfruit','peach','pear','persimmon','physalis','plum/prune','pineapple','pomegranate','raspberry','rambutan','star fruit','strawberry'];
+
+$(function(){
+  let list = $("#list");
+
+  function appendList(word){
+    let item = $(('<li class="list">')).append(word);
+    list.append(item);
+  }
+
+  function editElement(element){
+    let result = "^" + element;
+    return result;
+  }
+
+  $('#submit').on("click", function(){
+    let input = $("#keyword").val();
+    let inputs = input.split(" ");
+    let newInput = inputs.map(editElement);
+    let reg = RegExp(newInput.join("|"));
+
+    $.each(fruits, function(i, fruit){
+      if (fruit.match(reg){
+        appendList(fruit);
+      }
+    });
+
+    if ($(".list").length === 0) {
+      appendList("一致する果物はありませんでした。")
+    }
+  });
+});

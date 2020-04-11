fde6540a0055b6a1f9d64a1e99938527954e6261
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-04-11 14:22:28 +0900

    Update todo2.js

--- practice/todo2.js
+++ practice/todo2.js
@@ -166,9 +166,56 @@ const chat = [
 let chatCount = 0;
 
 function output(val, person){
+  const ul = document.getElementById('chat-ul');
+  const li = document.get.createElement('li');
+
+  const div = document.createElement('div');
+  div.textContent = val;
+
+  if (person === 'me') {
+    li.classList.add('chat-right');
+    ul.appendChild(li);
+    li.appendChild(div);
+  }else if (person === 'other') {
+    chatBtn.disabled = true;
+    setTimeout( ()=> {
+      chatBtn.disabled = false;
+      li.classList.add('chat-left');
+      ul.appendChild(li);
+      li.appendChild(div);
+      chatCount++;
+    }, 2000);
+  }
 }
 
 const chatBtn = document.getElementById('chat-button');
 const inputText = document.getElementById('chat-input');
 
-fu
+
+function btnFunc(){
+  if(!inputText.value) return false;
+
+  output(inputText.value, 'me');
+
+  setTimeout( ()=> {
+    inputText.value = '';
+  },1);
+
+  switch(chatCount) {
+    case 2:
+      output('Hi, ' + inputText.value + '!', 'other');
+      setTimeout( ()=>{
+        output(chat[2],'other');
+      }, 2000);
+      break;
+
+    case 4:
+      output(chat[3][Math.floor(Math.random() * chat[3].length)], 'other');
+      break;
+  }
+}
+
+output(chat[0], 'other');
+setTimeout( ()=>{
+  output(chat[1],'other');
+}, 2000);

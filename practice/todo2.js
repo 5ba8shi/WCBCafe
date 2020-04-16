1dbffd9dd0e5bab2d074fccac57c447e98f2664d
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-04-16 12:24:44 +0900

    Update todo2.js

--- practice/todo2.js
+++ practice/todo2.js
@@ -207,3 +207,26 @@ $(function(){
 });
 
 
+
+window.addEventListener("load", function(){
+  let tabs = document.getElementsByClassName("menu_item");
+
+  tabsAry = Array.prototype.slice.call(tabs);
+
+  function tabSwitch(){
+    document.getElementsByClassName("active")[0].classList.remove("active");
+
+    this.classList.add("active");
+
+    document.getElementsByClassName("show")[0].classList.remove("show");
+
+    const index = tabsAry.indexOf(this);
+
+    document.getElementsByClassName("content")[index].classList.add("show");
+  }
+
+  tabsAry.forEach(function(value){
+    value.addEventListner("click", tabSwitch);
+  });
+});
+

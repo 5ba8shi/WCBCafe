3edf26f0b5a9258521e227d289b381ff41d7aa73
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-04-18 19:28:21 +0900

    Update todo2.js

--- practice/todo2.js
+++ practice/todo2.js
@@ -69,3 +69,91 @@ output(chat[0], 'other');
 setTimeout( ()=> {
   output(chat[1], 'other');
 }, 2000);
+
+
+
+function fizzbuzz(num){
+  if(num %  3 === 0 && num % 5 === 0) {
+    return 'fizzbuzz';
+  } else if(num % 3 === 0){
+    return 'fizz';
+  } else if(num % 5 === 0){
+    return 'buzz';
+  } else {
+    return num;
+  }
+}
+
+let i = 1;
+while(i <= 30){
+  console.log(fizzbuzz(i));
+  i += 1;
+}
+
+
+$(function(){
+  $('form').on('submit', function(e){
+    let output = '';
+    let checkboxes = $(this).find('input[type="checkbox"]');
+
+    checkboxes.each(function(i, checkbox){
+      checkbox = $(checkboxes[i]);
+      if (checkbox.prop('checked')){
+        output += checkbox.attr('value') + '\n';
+      }
+    });
+
+    e.preventDefault();
+    alert('あなたが選んだ果物:')
+  })
+})
+
+
+$(function(){
+  let tabs = $(".menu_item");
+  function tabSwitch(){
+    $('active').removeClass('active');
+    $(this).addClass('active');
+    const index = tabs.index(this);
+
+    $(".content").removeClass("show").eq(index).addClass("show");
+  }
+  tabs.click(tabSwitch);
+})
+
+navigator.geolocation.getCurrentPosition(success, fail);
+
+function success(pos){
+}
+
+function fail(error){
+}
+
+function utcToJSTime(utcTime){
+  return utcTime * 1000;
+}
+
+function ajaxRequest(lat, long){
+  const url = 'https://api.openweathermap.org/data/2.5/forecast';
+  const appId = '保存しておいたAPI KEY';
+
+  $.ajax({
+    url: url,
+    data: {
+      appid: appId,
+      lat: lat,
+      lon: long,
+      units: 'metric',
+      lang: 'ja'
+    }
+  })
+  .done(function(data){
+    $('#place').text(data.city.name + ', ' + data.city.country);
+
+    data.list.forEach(function(forecast, index){
+      const dateTime = newDate(utcToJSTime(forecast.dt));
+      const month = dateTime.getMonth() + 1;
+      const date = dateTime.getDate();
+    }
+  })
+}

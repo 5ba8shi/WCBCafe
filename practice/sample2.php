741be2f506a9b49da6cab94df3437e270c3a6ba4
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-04-23 19:09:52 +0900

    ９

--- practice/sample2.php
+++ practice/sample2.php
@@ -3,3 +3,42 @@
 }
 ?>
 
+<?php for($i = 1; $i < 4; $i++){
+  echo "<option value='{$i}'>{$i}</option>";
+}
+?>
+
+<?php for($i = 6; $i <= 100; $i++){
+  echo"<option value='{$i}'>{$i}</option>";
+}
+?>
+
+<?php
+$data = array('東京','大阪','京都');
+foreach($data as $value){
+  echo $value[0];
+}
+?>
+
+<?php
+$data 
+
+
+<?php for($i = 6; $i <= 100; $i++){
+  echo"<option value='{$i}'>{$i}</option>";
+}
+?>
+
+
+<div class="form-item">お問い合わせの種類</div>
+<?php 
+  $types = array('Progateに関するお問い合わせ', 'Progateに対する意見', '採用に関するお問い合わせ', '取材・メディア関連のお問い合わせ', '料金に関するお問い合わせ', 'その他');
+ ?>
+<!-- この下にselectタグを書いていきましょう -->
+<select name="category">
+  <option value="未選択">選択してください</option>
+   <?php
+     foreach ($types as $type) {
+      echo "<option value='{$type}'>{$type}</option>";
+     }
+   ?>
---
+++ practice/todo2.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Nav from './Components/Nav';
+import Title from './Components/Title';
+import About from './Components/AboutMe';
+import Works from './Components/Works';
+import Contact from './Components/Contact';
+
+class App extends Component {
+  render (){
+    return(
+      <div className="app">
+        <Nav />
+        <Title />
+        <About />
+        <Works />
+        <Contact />
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
+import React, { Component } from 'react';
+import { Layout, Header, Navigation, Drawer } from 'react-mdl';
+import { Link } from 'react-scroll';
+
+class Nav extends Component {
+  render() {
+    return (
+      <div className="demo-big-content">
+        <Layout>
+          <Header className="header" style = {{ fontWeight: 'bold', fontSize: '45px'}} title= " " scroll>
+            <Navigation className="navigation">
+              <Link
+                activeClass="active"
+                to="title"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration= {800}
+                >Top</Link>
+            </Navigation>
+          </Header>
+        </Layout>
+      </div>
+    )
+  }
+}
+
+window.addEventListener("load", function(){
+  let btn = document.querySelector("button#Button");
+  btn.addEventListener("click", function(){
+    console.log("Hello world");
+  });
+
+  let btn2 = document.querySelector("button#button2");
+  let changeText = document.querySelector("p");
+
+  btn2.addEventListener("click", function(){
+    changeText.innerHTML = '変更されました';
+  });
+
+  let btn3 = document.querySelector("#Button3");
+
+  btn3.addEventListener("click", function(){
+    changeText.classList.add("red");
+  });
+
+  let btn4 = document.querySelector("#Button4");
+  let obj = document.querySelector("div");
+
+  btn4.addEventListener("click", function(){
+    obj.classList.remove("blue");
+  });
+});
+
+window.addEventListener("load", function(){
+  let btn = document.querySelector("button#Button");
+  
+  btn.addEvent
+})

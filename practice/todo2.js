8fe096b4a23b8c9ead8f159cb0c90fd7bcceefaf
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-04-25 22:02:55 +0900

    Update todo2.js

--- practice/todo2.js
+++ practice/todo2.js
@@ -181,3 +181,16 @@ for(let item of todo){
   const li = `<li>${item}</li>`;
   document.getElementById('list').insertAdjacentHTML('beforeend', li);
 }
+
+
+$(box).find('check').addClass('crowded');
+
+
+$('.check').on('click', function(){
+  if($(this).hasClass('crowded')) {
+    $(this).text('残席わずか').addClass('red');
+  } else {
+    $(this).text('お席あります').addClass('green');
+  }
+})
+

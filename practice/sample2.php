4123eaaa1896a2f022bf50970dd7bc73806e0f8b
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-05-12 07:41:44 +0900

    123

--- practice/sample2.php
+++ practice/sample2.php
@@ -165,3 +165,14 @@ $input_line = trim(fgets(STDIN));
 ?>
 
 hello = 2 , world = 5
+
+
+<?php
+$input_line = trim(fgets(STDIN));
+for ($i = 0; $i < $input_line; $i++) {
+  $s = trim(fgets(STDIN));
+  $s = str_replace(array("\r\n","\r","\n"), '', $s);
+  $s = explode(" ", $s);
+  echo "hello = ".$s[0]." , world = ".$s[1]."\n";
+}
+?>
--- practice/todo2.js
+++ practice/todo2.js
@@ -6,3 +6,110 @@ let myChart = new Chart(ctx, {
     labels:
   }
 })
+
+window.addEventListener("load", function(){
+  let btn = document.querySelector("button#Button");
+  
+  btn.addEventListener("click", function(){
+    console.log("Hello world");
+  });
+
+  let btn2 = document.querySelector("button#Button2");
+  let changeText = document.querySelector("p");
+
+  btn.addEventListener("click", function(){
+    changeText.innerHTML = '変更されました';
+  });
+
+
+  let btn3 = document.querySelector("#Button4");
+
+  let obj = document.querySelector("div");
+
+  btn4.addEventListener("click", function(){
+    obj.classList.remove("blue");
+  });
+});
+
+
+window.addEventListener("load", function(){
+  let btn = document.querySelector("button#Button");
+
+  btn.addEventListener("click", function(){
+    console.log("Hello world");
+  });
+
+  let btn2 = document.querySelector("button#Button2");
+});
+
+window.addEvenetListener("load", function(){
+  let btn = document.querySelector("button#Button");
+
+  btn.addEventListener("click", function(){
+    console.log("Hello world");
+  });
+  
+  let btn2 = document.querySelector("button#Button2");
+  let changText = document.querySelector("p");
+  
+  btn2.addEventListener("click", function(){
+    changeText.inner(HTML) = '変更されました';
+  });
+
+  let btn3 = document.querySelector("#Button3");
+
+  btn3.addEventListener("click", function(){
+    changeText.classList.add("red");
+  });
+
+  let btn4 = document.querySelector("#Button4");
+  let obj = document.querySelector("div");
+
+  btn4.addEventListener("click", function(){
+    obj.classList.remove("blue");
+  });
+});
+
+
+window.addEventListener("load", function(){
+  let btn = document.querySelector("button#Button");
+
+  btn.addEventListener("click", function(){
+    console.log("Hello world");
+  });
+  
+  let btn2 = document.querySelector("button#Button2");
+  let changeText = document.querySelector("p");
+  
+  btn2.addEventListener("click", function(){
+    changeText.innerHTML = '変更されました';
+  });
+
+  let btn3 = document.querySelector("#Button3");
+
+  btn3.addEventListener("click", function(){
+    changeText.classList.add("red");
+  });
+
+  let btn4 = document.querySelector("#Button4");
+
+  let obj = document.querySelector("div");
+
+  btn4.addEventListener("click", function(){
+    obj.classList.remove("blue");
+  });
+});
+
+const chat = [
+  'Hello'
+]
+
+import React from "react"
+import styled from "styled-components"
+import InViewMonitor from "react-inview-monitor"
+
+export const Up = ({ children, delay, margin = "-20%" }) => (
+  <InViewMonitor childPropsInView={{ isActive: true }} intoViewMargin={margin}>
+    <FadeInUp delay={delay}>{children}</FadeInUp>
+  </InViewMonitor>
+)

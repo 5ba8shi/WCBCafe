f2fe7efba021e660d2f77871fbceb465ee34be28
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-05-20 14:28:02 +0900

    Update todo2.js

--- practice/todo2.js
+++ practice/todo2.js
@@ -150,10 +150,65 @@ window.addEventListener("load", function(){
   tabsAry = Array.prototype.slice.call(tabs);
 
   function tabSwitch() {
-    document.getE
+    document.getElementsByClassName("active")[0].classList.remove("active");
+
+    this.classList.add("active");
+
+    document.getElementsByClassName("show")[0].classList.remove("show");
+
+    const index = tabsAry.indexOf(this);
+
+    document.getElementsByClassName("content")[index].classList.add("show");
   }
 
   tabsAry.forEach(function(value) {
     value.addEventListener("click", tabSwitch);
   });
 });
+
+
+$(function(){
+
+  $('form').on('submit', function(e){
+    let output = '';
+    let checkboxes = $(this).find('input[type="checkbox"]');
+
+    checkboxes.each(function(i, checkbox){
+      checkbox = $(checkboxes[i]);
+      if(checkbox.prop('checked')){
+        output += checkbox.attr('value') + '\n';
+      }
+    });
+    
+    e.preventDefault();
+    alert('あなたが選んだ果物:\n' + output);
+  });
+});
+
+
+
+function MailCheck( mail ) {
+  var mail_regex1 = new RegExp( '(?:[-!#-\'*+/-9=?A-Z^-~]+\.?(?:\.[-!#-\'*+/-9=?A-Z^-~]+)*|"(?:[!#-\[\]-~]|\\\\[\x09 -~])*")@[-!#-\'*+/-9=?A-Z^-~]+(?:\.[-!#-\'*+/-9=?A-Z^-~]+)*' );
+  var mail_regex2 = new RegExp( '^[^\@]+\@[^\@]+$' );
+  if( mail.match( mail_regex1 ) && mail.match( mail_regex2 ) ) {
+      // 全角チェック
+      if( mail.match( /[^a-zA-Z0-9\!\"\#\$\%\&\'\(\)\=\~\|\-\^\\\@\[\;\:\]\,\.\/\\\<\>\?\_\`\{\+\*\} ]/ ) ) { 
+        return false; 
+      }
+      // 末尾TLDチェック（〜.co,jpなどの末尾ミスチェック用）
+      if( !mail.match( /\.[a-z]+$/ ) ) { 
+        return false; 
+      }
+      return true;
+  } else {
+      return false;
+  }
+}
+
+
+/^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$
+
+^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$
+
+
+^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$

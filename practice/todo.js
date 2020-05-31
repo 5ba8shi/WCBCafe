a415903bdfaae414db95931ef17dbbcbed98d9f7
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-05-31 20:28:18 +0900

    12345

--- practice/todo.js
+++ practice/todo.js
@@ -1,57 +1,28 @@
-// JavaScript側のコード
-function validate(objTd, objId){
+window.addEventListener("load", function(){
+    let btn = document.querySelector("button#Button");
 
-    var objCtrl = document.getElementById(objId);
-    var objRegExp = null;
-    var msg = "";
+    btn.addEventListener("click", function(){
+        console.log("Hello world");
+    });
 
-    switch(objId)
-    {
-        // 本当は他にもあるけど、今回は省略。
-        case 'form_name':
-            if("" == objCtrl.value) {
-                msg = "お名前は必須です。";
-            }else{
-                if (objCtrl.value.length > 10) {
-                    msg = "お名前は10文字以内で入力して下さい。";
-                }else{
-                    // 正規表現による文字チェック
-                    objRegExp = new RegExp(/^[\sa-zA-Z0-9!#$%&=-~^@+.ぁ-んーァ-ヶー一-龠１-９Ａ-Ｚａ-ｚ！＃＄％＆＝－～＾＠＋．、。？-]+$/);
-                    if (!objRegExp.test(objCtrl.value)) {
-                        msg = "お名前の値が不正です。";
-                    }
-                }
-            }
-        break;
-    }
+    let btn2 = document.querySelector("button#Button2");
+    let changeText = document.querySelector("p");
 
-  displayErrorInfo(objTd, objId, msg);
-}
+    btn2.addEventListener("click", function(){
+        changeText.innerHTML = '変更されました';
+    });
 
+    let btn3 = document.querySelector("#Button3");
 
+    btn3.addEventListener("click", function(){
+        changeText.classList.add("red");
+    });
 
-function displayErrorInfo(objTd,objId, msg){
+    let btn4 = document.querySelector("#Button4");
 
-    var objParent = document.getElementById(objTd);
-    var objCtrl = document.getElementById(objId);
-    // objIdの文字を末尾に追加しているのは、他のコントロールで使用した際IDがかぶるのを防ぐため。
-    var objChild = document.getElementById('form_childDiv' + objId);
-    // 同じエラーメッセージが何個も表示されないように、既存のエラーメッセージは削除する。
+    let obj = document.querySelector("div");
 
-    if(null != objChild){
-      objParent.removeChild(objChild);
-    }
-
-    if("" != msg) {
-        // エラーが有る場合
-        objCtrl.style.backgroundColor = "#FADBDA";
-        var errorNode = document.createElement("div");
-        errorNode.id = "form_childDiv" + objId;
-        errorNode.style.color = "#FF0000";
-        errorNode.innerText = msg;
-        objParent.appendChild(errorNode);
-    }else{
-        // エラーがない場合
-        objCtrl.style.backgroundColor = "#FFFFFF";
-    }
-}
+    btn4.addEventListener("click", function(){
+        obj.classList.remove("blue");
+    });
+});

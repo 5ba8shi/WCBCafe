6f91dc59dfe26d69e966edc224b527c7458c9fe5
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-05-31 16:32:44 +0900

    1234

--- practice/sample2.php
+++ practice/sample2.php
@@ -1,198 +1,40 @@
+@extends('layouts.app')
 
-<?php echo'<h2>初級</h2>'; ?>
+@section('content')
+<div class="container">
+  <div class="row justify-content-center">
+    
+    <div class="card">
+      <div class="card-header">{{__('Confirm Password') }}</div>
 
+      <div class="card-body">
+        {{ __('Please confirm your password before continuing.')}}
 
-<?php
-  echo 'こんにちは';
-  echo 'PHPを学ぼう';
-?>
+        <form method ="POST" action="{{ route('password.confirm')}}">
+          @csrf
 
-<?php echo 'Hello, World';?>
+          <div class="form-group row">
+            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
-<?php
-echo 7+3;
-echo 10-5;
-echo 3*5;
-echo 4%4;
-?>
+            <div class="col-md-6">
+              <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
-<?php
-$name = 'Bob';
+              @error('password')
+                <span class="invalid-feedback" role="alert">
+                  <strong>{{ $message }}</strong>
+                </span>
+              @enderror
+            </div>
+          <div>
+          <div class="form-group row md-0">
+            <div class="col-md-8 offset-md-4">
+              <button type="submit" class="btn btn-primary">
+                {{ __('Confirm Password') }}
+              </button>
 
-echo $name;
-?>
-
-<?php
-$length = 7;
-$area = $length * $length;
-
-echo $area;
-?>
-
-<?php
-$area = 7*7;
-echo $area;
-?>
-
-<?php
-$num = 3;
-echo $num;
-$num = 5;
-echo $num;
-?>
-
-<?php $num =3;
-echo $num;
-$num = 5;
-echo $num;
-?>
-
-<?php
-$x = 1;
-$x += 10;
-
-echo $x;
-?>
-
-$x += 10;
-
-<?php
-$x +=1
-?>
-
-<?php
-$lesson = 'PHP';
-echo $lesson.'入門';
-
-$level = '入門';
-echo $lesson.$level;
-
-?>
-
-<?php
-$name = 'にんじゃ';
-$name .= 'わんこ';
-
-echo $name;
-?>
-
-<?php
-$name = 'にんじゃわんこ';
-echo "こんにちは、{$name}さん";
-
-echo 'こんにちは、{$name}さん';
-?>
-
-<?php
-$name ='にんじゃわんこ';
-echo "こんにちは、{$name}さん";
-?>
-
-<?php
-$name = 'にんじゃわんこ';
-echo "こんにちは！{$name}";
-?>
-
-<?php
-$x = 20;
-if($x > 10){
-  echo '$xは10より大きい';
-}
-
-$x = 20;
-if($x > 30){
-  echo '$xは30より大きい';
-}
-
-$i++
-$i--
-?>
-
-<?php
-if(true){
-  echo '条件はtrueです';
-}
-?>
-
-<?php
-$x=20;
-if($x > 30){
-  echo '$xは30より大きい';
-} else if($x > 10){
-  echo '$xは10より大きい';
-} else {
-  echo 'いずれも当てはまらない';
-}
-?>
-
-<?php
-$x = 20;
-if ($x == 30){
-  echo '$xは30です';
-} else {
-  echo'$xは30ではありません';
-}
-?>
-
-
-
-<?php
-$x = 20;
-if($x > 30){
-  echo '$xは30より大きい';
-} elseif($x >= 20){
-  echo '$xは30以下20以上';
-} else{
-  echo'$xは20より小さい';
-}
-?>
-
-
-
-
-2 5
-
-
-
-<?php
-$input_line = trim(fgets(STDIN));
-
-  $s = trim(fgets(STDIN));
-  $s = str_replace(array("\r\n","\r","\n"), '', $s);
-  $s = explode(" ", $s);
-  echo "hello = ".$s[0]." , world = ".$s[1]."\n";
-
-?>
-
-hello = 2 , world = 5
-
-
-<?php
-$input_line = trim(fgets(STDIN));
-for ($i = 0; $i < $input_line; $i++) {
-  $s = trim(fgets(STDIN));
-  $s = str_replace(array("\r\n","\r","\n"), '', $s);
-  $s = explode(" ", $s);
-  echo "hello = ".$s[0]." , world = ".$s[1]."\n";
-}
-?>
-
-<?php
-
-@section('sidebr')
-
-@show
-
-  <div class="container">
-    @yield
-  </div>
-</body>
-?>
-
-
-  @extends('layouts.app')
-
-  @section('title', 'Page Title')
-
-  
+              @if (Route::has('password.request'))
+                <a class="btn btn-link" href="{{  route('password.request') }}">
+                  {{ __('Forgot Your Password?')}}
+                </a>
+              @endif
 
--- practice/todo2.js
+++ practice/todo2.js
@@ -8,3 +8,26 @@ if (String.fromCharCode(e.keyCode) === currentWord[currentLocation].toUpperCase(
   miss++;
   missLabel.innerHTML = miss;
 }
+
+if (String.fromCharCode(e.keyCode) === currentWord[currentLocation].toUpperCase()) {
+  currentLocation++;
+
+  score++;
+  scoreLabel.innerHTML = score;
+} else {
+  miss++;
+  missLabel.innerHTML = miss;
+}
+
+function updateTimer() {
+  timerId = setTimeout(function(){
+    timer--;
+    timerLabel.innerHTML = timer;
+
+    if (timer <= 0){
+      var accuracy = (score + miss) === 0 ? '0.00' : ((score / (score + miss)) * 100). toFixed(2);
+      result.innerText = (score + )
+      clear
+    }
+  })
+}

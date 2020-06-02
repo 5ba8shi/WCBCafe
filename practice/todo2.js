9e9d383736e216e60ece44aa2667bd74fea59f07
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-06-02 10:53:44 +0900

    Update todo2.js

--- practice/todo2.js
+++ practice/todo2.js
@@ -1,33 +1,21 @@
-if (String.fromCharCode(e.keyCode) === currentWord[currentLocation].toUpperCase()) {
-  currentLocation++;
+drawnCards =[];
+yourHand = [];
+opponentsHand = [];
+faceValue = new Map();
+faceValue.set('a', 1);
 
-  score++;
-  scoreLabel.innerHTML = score;
-} else {
+drawnCards=[];
+yourHand = [getCard(), getCard()];
+opponentsHand = [getCard(), getCard()];
 
-  miss++;
-  missLabel.innerHTML = miss;
-}
+gameOver = false;
+dealerTurn = false;
+gameWin = false;
+gameLose = false;
+push = false;
+showDealerCard = false;
+aiInfo = "";
 
-if (String.fromCharCode(e.keyCode) === currentWord[currentLocation].toUpperCase()) {
-  currentLocation++;
+draw();
 
-  score++;
-  scoreLabel.innerHTML = score;
-} else {
-  miss++;
-  missLabel.innerHTML = miss;
-}
-
-function updateTimer() {
-  timerId = setTimeout(function(){
-    timer--;
-    timerLabel.innerHTML = timer;
-
-    if (timer <= 0){
-      var accuracy = (score + miss) === 0 ? '0.00' : ((score / (score + miss)) * 100). toFixed(2);
-      result.innerText = (score + )
-      clear
-    }
-  })
-}
+function 

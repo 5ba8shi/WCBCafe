19bfa9a1a6a3c69b4066f194032e81313cf12c64
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-06-06 14:58:49 +0900

    Update todo.js

--- practice/todo.js
+++ practice/todo.js
@@ -65,3 +65,32 @@ function draw() {
   };
 
 };
+
+function getHandValue(hand) {
+  cards = [];
+
+  for(i = 0; i < hand.length; i++) {
+    cards[i] = faceValue.get(hand[i].substring(0, hand[i].length - 1));
+  };
+
+  aceCount = 0;
+  total = 0;
+
+   for(i = 0; i < cards.length; i++) {
+     if(cards[i] == 1){
+       aceCount++;
+     } else {
+       total += cards[i];
+     };
+   };
+
+   if(aceCount == 0) {
+     return total;
+   } else {
+     if(total <= 10) {
+       return (total + 11) + (aceCount - 1);
+     } else {
+       return total + aceCount;
+     };
+   };
+};

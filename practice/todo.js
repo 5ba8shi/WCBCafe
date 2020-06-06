36d17a0de56e8a97699ea38de8ec4e570b59574e
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-06-06 14:09:37 +0900

    Update todo.js

--- practice/todo.js
+++ practice/todo.js
@@ -22,3 +22,46 @@ if(getHandValue(yourHand) <= 21) {
   you.appendChild(para);
 };
 
+
+function draw() {
+  opponent = document.getElementsByClassName("opponent")[0];
+  you = document.getElementsByClassName("you")[0];
+  opponent.innerHTML = "<p>ディーラーのカード</p>";
+  you.innerHTML = "<p>あなたのカード</p>";
+
+  for(i = 0; i < opponentsHand.length; i++){
+    img = new Image();
+
+    if(!showDealerCard && i != 0) {
+      img.src = '../img/card_back.png';
+    } else {
+      img.src = '../img/' + opponentsHand[i] + '.jpg';
+    };
+    opponent.appendChild(img);
+  };
+
+  parao = document.createElement("p");
+  if(showDealerCard) {
+    result = document.createTextNode("ディーラーのカード:" + getHandValue(opponentsHand) + aiInfo);
+    parao.appendChild(result);
+    opponent.appendChild(parao);
+  };
+
+  for(i = 0; i < yourHand.length; i++) {
+    img = new Image();
+    img.src = '../img/' + yourHand[i] + '.jpg';
+    you.appendChild(img);
+  };
+
+  para = document.createElement("P");
+  if(getHandValue(yourHand) <= 21) {
+    result = document.createTextNode("あなたの合計: " + getHandValue(yourHand));
+    para.appendChild(result);
+    you.appendChild(para);
+  } else { 
+    result = document.createTextNode("あなたの合計: アウト");
+    para.appendChild(result);
+    you.appendChild(para);
+  };
+
+};

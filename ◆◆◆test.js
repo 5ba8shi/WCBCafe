7cbc55ea8627d627eba3d97c7becfe83d749b0ac
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-08-14 16:46:32 +0900

    Update ◆◆◆test.js

---
+++ ◆◆◆test.js
@@ -0,0 +1,28 @@
+window.addEventListener("load", function(){
+  let btn = document.querySelector("button");
+
+  btn.addEventListener("click", function(){
+    console.log("Hello World");
+  });
+
+  let btn2 = document.querySelector("button#Button2");
+  let changeText = document.querySelector("p");
+
+  btn2.addEventListener("click", function(){
+    changeText.innerHTML = '変更されました。';
+  });
+
+  let btn3 = document.querySelector("#Button3");
+
+  btn3.addEventListener("click", function() {
+    changeText.classList.add("red");
+  });
+
+  let btn4 = document.querySelector("#Button4");
+
+  let obj = document.querySelector("div");
+
+  btn4.addEventListener("click", function() {
+    obj.classList.remove("blue");
+  });
+});

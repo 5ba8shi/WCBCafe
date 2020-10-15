3809f4a4fe1479cd01ff889ed3fba84bbed34378
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-10-15 11:57:45 +0900

    Update ◆◆◆test.js

---
+++ ◆◆◆test.js
@@ -0,0 +1,94 @@
+window.addEventListener("load", function() {
+
+  const chat = [
+    'Hello ! Welcome to AI chat !',
+    'What is your name ?',
+    'How are you today ?',
+    [['Alright !'], ['Oh really!'], ['Ok!']],
+    'Where are you from?',
+    [['Perfect !!'], ['Great!!'], ['I want live there!'], ['I think crazy place!!'], ['I have been there. When I was kids.'],['Fuck You!!']]
+  ];
+
+  let chatCount = 0;
+
+  function output(val, person) {
+    const ul = document.getElementById('chat-ul');
+    const li = document.createElement('li');
+
+    const div = document.createElement('div');
+    div.textContent = val;
+
+    if(person === 'me') {
+      li.classList.add('chat-right');
+      ul.appendChild(li);
+      li.appendChild(div);
+    } else if (person === 'other') {
+      chatBtn.disabled = true;
+      setTimeout( () => {
+        chatBtn.disabled = true;
+        li.classList.add('chat-left');
+        ul.appendChild(li);
+        li.appendChild(div);
+
+        chatCount++;
+      }, 2000);
+    }
+  }
+
+  const chatBtn = document.getElementById('chat-button');
+  const inputText = document.getElementById('chat-input');
+
+  chatBtn.addEventListener('click', function(){
+    if(!inputText.vlaue) return false;
+
+    output(inputText.value, 'me');
+
+    setTimeout( () => {
+      inputText.value = '';
+    }, 1);
+
+    switch(chatCount) {
+
+      case 2:
+        output('Hi, ' + inputText.value + ' !', 'other');
+
+        setTimeout( () => {
+          output(chat[2], 'other');
+        }, 2000);
+        break;
+
+      
+      case 4:
+        output(chat[3][Math.floor(Math.random() * chat[3].length)], 'other');
+        setTimeout( () => {
+          output(chat[4], 'other');
+        }, 2000);
+        break;
+      
+      case 6:
+        output(chat[5][Math.floor(Math.random()* chat[5].length)], 'other');
+        break;
+    }
+  });
+
+  output(chat[0], 'other');
+
+  setTimeout( ()=> {
+    output(chat[])
+  })
+
+})
+
+output(chat[0], 'other');
+
+setTimeout( ()=> {
+  output(chat[1], 'other');
+}, 2000);
+
+function output(val, person){
+  const ul = document.getElementById('chat-ul');
+  const li = document.createElement('li');
+
+  const div = document.createElement('div');
+  div.textContent = val;
+}

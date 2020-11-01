416d41af6f1d9691cfa646fbf79866bf96a89a29
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-11-01 12:25:39 +0900

    12

--- ◆◆◆test.js
+++ ◆◆◆test.js
@@ -22,15 +22,15 @@ window.addEventListener("load", function() {
       li.classList.add('chat-right');
       ul.appendChild(li);
       li.appendChild(div);
-    } else if (person === 'other') {
+    } else if(person === 'other') {
       chatBtn.disabled = true;
-      setTimeout( () => {
-        chatBtn.disabled = true;
+      setTimeout( ()=> {
+        chatBtn.disabled = false;
         li.classList.add('chat-left');
         ul.appendChild(li);
         li.appendChild(div);
 
-        chatCount++;
+        chatCout++;
       }, 2000);
     }
   }
@@ -38,26 +38,24 @@ window.addEventListener("load", function() {
   const chatBtn = document.getElementById('chat-button');
   const inputText = document.getElementById('chat-input');
 
-  chatBtn.addEventListener('click', function(){
-    if(!inputText.vlaue) return false;
+  chatBtn.addEventListener('click', function() {
+    if(!inputText.value) return false;
 
     output(inputText.value, 'me');
 
-    setTimeout( () => {
+    setTimeout( ()=> {
       inputText.value = '';
     }, 1);
 
     switch(chatCount) {
-
       case 2:
         output('Hi, ' + inputText.value + ' !', 'other');
-
-        setTimeout( () => {
+        setTimeout( ()=> {
           output(chat[2], 'other');
+
         }, 2000);
         break;
 
-      
       case 4:
         output(chat[3][Math.floor(Math.random() * chat[3].length)], 'other');
         setTimeout( () => {
@@ -65,46 +63,37 @@ window.addEventListener("load", function() {
         }, 2000);
         break;
       
-      case 6:
-        output(chat[5][Math.floor(Math.random()* chat[5].length)], 'other');
-        break;
+      case 6;
+        output(chat[5][Math.floor(Math.floor(Math.random()* chat[5].length))], 'other');
+
     }
   });
 
   output(chat[0], 'other');
 
   setTimeout( ()=> {
-    output(chat[])
+    output(chat[1], 'other');
+  }, 2000);
+
+});
+
+window.addEventListener("load", function() {
+  let btn = document.querySelector("buttton#Button");
+
+  btn.addEventListener("click", function() {
+    console.log("Hello World");
   })
 
+  let btn2 = document.querySelector("button#Button2");
+  let changeText = document.querySelector("p");
+
+  btn2.addEventListener("click", function() {
+    changeText.innerHTML = '変更されました';
+  });
+
+  let btn3 = document.querySelector("#Button3");
+
+  btn3.addEventListener("click", function() {
+    changeText.classList.add("red");
+  })
 })
-
-output(chat[0], 'other');
-
-setTimeout( ()=> {
-  output(chat[1], 'other');
-}, 2000);
-
-function output(val, person){
-  const ul = document.getElementById('chat-ul');
-  const li = document.createElement('li');
-
-  const div = document.createElement('div');
-  div.textContent = val;
-
-  if(person === 'me') {
-    li.classList.add('chat-right');
-    ul.appendChild(li);
-    li.appendChild(div);
-  } else if(person === 'other'){
-    chatBtn.disabled = true;
-    setTimeout( ()=> {
-      chatBtn.disabled = false;
-      li.classList.add('chat-left');
-      ul.appendChild(li);
-      li.appendChild(div);
-      chatCount++;
-    }, 2000);
-
-  }
-}

5957d1780fecfbe5997cda0a2111557c66fd5b48
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-11-01 14:12:28 +0900

    Update ◆◆◆test.js

--- ◆◆◆test.js
+++ ◆◆◆test.js
@@ -96,4 +96,12 @@ window.addEventListener("load", function() {
   btn3.addEventListener("click", function() {
     changeText.classList.add("red");
   })
+
+
+  let btn4 = document.querySelector("#Button4");
+  let obj = document.querySelector("div");
+
+  btn4.addEventListener("click", function() {
+    obj.classList.remove("blue");
+  })
 })

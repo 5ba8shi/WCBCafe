57b04243545b2f08f2a60d2dcb029af316beeb44
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-11-04 08:21:38 +0900

    Update ◆◆◆test.js

--- ◆◆◆test.js
+++ ◆◆◆test.js
@@ -24,3 +24,39 @@ document.getElementById('form').onsubmit = function(event) {
   const search = document.getElementById('form').word.value;
   document.getElementById('output').textContent = `「${search}」の検索...`;
 };
+
+async function callApi() {
+  const res = await fetch("https://jsondata.okiba.me/v1/json/0hxL6200611030517");
+
+  const jobs = await res.json();
+
+  const Filter1 = jobs.filter(job1 => {
+    return job1.grpId === 2;
+  });
+
+  Filter1.sort(function(a, b){
+    a = a.jobName.toString().toLowerCase();
+    b = b.jobName.toString().toLowerCase();
+    if(a < b){
+      return -1;
+    } else if(a > b){
+      return 1;
+    }
+    return 0;
+  });
+
+  const Filter2 = jobs.filter(job => {
+    return job1.grpId === 2;
+  });
+
+  Filter1.sort(function(a, b){
+    a = a.jobName.toString().toLowerCase();
+    b = b.jobName.toString().toLowerCase();
+    if(a < b){
+      return -1;
+    } else if(a > b){
+      return 1;
+    }
+      return 0;
+  });
+}

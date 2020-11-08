f3fddef0e89882e42ac9daf5120ad686ff29fdab
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-11-09 06:54:43 +0900

    Update ◆◆◆test.js

--- ◆◆◆test.js
+++ ◆◆◆test.js
@@ -61,3 +61,30 @@ async function callApi() {
   });
 }
 
+const gaol = new Date(2025, 4, 3);
+
+function recalc(due) {
+  const counter = countdown(goal);
+  document.getElementById('day').textContent = counter[0];
+  document.getElementById('hour').textContent = counter[1];
+  document.getElementById('min').textContent = String(counter[2]).padStart(2, '0');
+  document.getElementById('sec').textContent = String(counter[3]).padStart(2, '0');
+  refresh();
+}
+
+function refresh() {
+  setTimeout(recalc, 1000);
+}
+recalc();
+
+function countdown(due) {
+  const now = new Date();
+
+  const rest = due.getTime() - now.getTime();
+  const sec = Math.floor(rest / 1000) % 60;
+  const min = Math.floor(rest / 1000 / 60) % 60;
+  const hours = Math.floor(rest / 1000 / 60 / 60) % 24;
+  const count = [days, hours, min, sec];
+
+  return count;
+}

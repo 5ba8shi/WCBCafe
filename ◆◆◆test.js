1a562866fcd511193c69810171b763655af68c0f
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-11-13 06:19:01 +0900

    Update ◆◆◆test.js

--- ◆◆◆test.js
+++ ◆◆◆test.js
@@ -1,170 +1,76 @@
-window.addEventListener("load", function() {
-  let tabs = document.getElementsByClassName("menu_item");
+window.addEventListener("load", function(){
 
-  tabsAry = Array.prototype.slice.call(tabs);
+    //相手の返答内容
+    const chat = [
+      'Hello ! Welcome to AI chat !',
+      'What is your name ?',
+      'How are you today ?',
+      [['Alright !'], ['Oh really!'], ['Ok!']],
+      'Where are you from?',
+      [['Perfect !!'], ['Great!!'], ['I want live there!'], ['I think crazy place!!'], ['I have been there. When I was kids.'],['Fuck You!!']]
+    ];
 
-  function tabSwitch() {
-    document.getElementsByClassName("active")[0].classList.remove("active");
+    let chatCount = 0;
 
-    this.classList.add("active");
+    function output(val, person) {
+      const ul = document.getElementById('chat-ul');
+      const li = document.createElement('li');
 
-    document.getElementsByClassName("show")[0].classList.remove("show");
+      const div = document.createElement('div');
+      div.textContent = val;
 
-    const index = tabsAry.indexOf(this);
+      if(person === 'me'){
+        li.classList.add('chat-right');
+        ul.appendChild(li);
+        li.appendChild(div);
+      } else if (person === 'other') {
 
-    document.getElementsByClassName("content")[index].classList.add("show");
-  }
-  tabsAry.forEach(function(value) {
-    value.addEventListener("click", tabSwitch);
-  });
+        chatBtn.disabled = ture;
+        setTimeout( () => {
+          chatBtn.disabled = false;
+          li.classList.add('chat-left');
+          ul.appendChild(li);
+          li.appendChild(div);
+          chatCount++;
+        }, 2000);
+      }
+    }
+
+    const chatBtn = document.getElementById('chat-button');
+    const inputText = document.getElementById('chat-input');
+
+
+    chatBtn.addEventListener('click', function() {
+      if (!inputText.value) return false;
+
+      output(inputText.value, 'me');
+
+      setTimeout( ()=> {
+        inputText.value = '';
+      }, 1);
+
+      switch(chatCount) {
+        case 2:
+          output('Hi, ' + inputText.value + ' !', 'other');
+
+          setTimeout( ()=> {
+            output(chat[2], 'other');
+          }, 2000);
+          break;
+
+        case 4:
+          output(chat[3][Math.floor(Math.random() * chat[3].length)], 'other');
+          setTimeout( ()=> {
+            output(chat[4], 'other');
+          }, 2000);
+          break
+      }
+    });
+
+    output(chat[0], 'other');
+
+    setTimeout ( ()=> {
+      output(chat[1], 'other');
+    }, 2000);
+
 });
-
-document.getElementById('form').onsubmit = function(event) {
-  event.preventDefault();
-  const search = document.getElementById('form').word.value;
-  document.getElementById('output').textContent = `「${search}」の検索...`;
-};
-
-async function callApi() {
-  const res = await fetch("https://jsondata.okiba.me/v1/json/0hxL6200611030517");
-
-  const jobs = await res.json();
-
-  const Filter1 = jobs.filter(job1 => {
-    return job1.grpId === 2;
-  });
-
-  Filter1.sort(function(a, b){
-    a = a.jobName.toString().toLowerCase();
-    b = b.jobName.toString().toLowerCase();
-    if(a < b){
-      return -1;
-    } else if(a > b){
-      return 1;
-    }
-    return 0;
-  });
-
-  const Filter2 = jobs.filter(job => {
-    return job1.grpId === 2;
-  });
-
-  Filter1.sort(function(a, b){
-    a = a.jobName.toString().toLowerCase();
-    b = b.jobName.toString().toLowerCase();
-    if(a < b){
-      return -1;
-    } else if(a > b){
-      return 1;
-    }
-      return 0;
-  });
-}
-
-const gaol = new Date(2025, 4, 3);
-
-function recalc(due) {
-  const counter = countdown(goal);
-  document.getElementById('day').textContent = counter[0];
-  document.getElementById('hour').textContent = counter[1];
-  document.getElementById('min').textContent = String(counter[2]).padStart(2, '0');
-  document.getElementById('sec').textContent = String(counter[3]).padStart(2, '0');
-  refresh();
-}
-
-function refresh() {
-  setTimeout(recalc, 1000);
-}
-recalc();
-
-function countdown(due) {
-  const now = new Date();
-
-  const rest = due.getTime() - now.getTime();
-  const sec = Math.floor(rest / 1000) % 60;
-  const min = Math.floor(rest / 1000 / 60) % 60;
-  const hours = Math.floor(rest / 1000 / 60 / 60) % 24;
-  const count = [days, hours, min, sec];
-
-  return count;
-}
-
-const lang = document.querySelector('html').lang:
-
-  if(lang === 'ja') {
-    document.querySelector('option[value="index.html"]').selected = true;
-  } else if(lang === 'en') {
-    document.querySelector('option[value="index-en.html"]').selected = true;
-  } else if (lang === 'zh') {
-    document.querySelector('option[value="index-zh.html"]').selected = true;
-  }
-
-  document.getElementById('form').select.onchange = function() {
-    location.href = document.getElementById('form').select.value;
-  }
-
-document.getElementById('pi').textContent = Math.PI;
-document.getElementById('floor').textContent = Math.floor(Math.PI);
-
-function point(num, digit){
-  const mover = 10 ** digit;
-  return Math.floor(num * mover) / mover;2309
-
-}
-
-document.getElementById('output').textContent = point(Math.PI, 2);
-
-let enemy = 100;
-let count = 0;
-
-window.alert('戦闘スタート');
-
-while(enemy > 0) {
-  const attack = Math.floor(Math.random() * 30) + 1;
-  console.log('モンスターに' + attack + 'のダメージ！');
-  enemy -= attack;
-  count += 1;
-}
-console.log(count + '回でモンスターを倒した！');
-
-function total(price) {
-  const tax = 0.1;
-  return price + price * tax;
-}
-
-console.log('コーヒーメーカーの値段は' + total(8000) + '円(税込み）です。');
-document.getElementById('output').textContent = 'コーヒーメーカーの値段は' + total(8000) + '円(税込)です。';
-
-
-let todo = ['デザインカンプ作成', 'データ整理', '勉強会申し込み', '牛乳買う'];
-todo.push('歯医者に行く');
-for(let item of todo) {
-  const li = `<li>${item}</li>`;
-  document.getElementById('list').insertAdjacentHTML('beforeend', li);
-}
-
-
-let jsbook = {title: 'JavaScript入門', pric: 2500, stock:3};
-
-document.getElementById('title').textContent = jsbook.title;
-document.getElementById('price').textContent = jsbook.price + '円';
-document.getElementById('stock').textContent = jsbook.stock;]
-
-
-
-const agree = Cookie.get('cookie-agree');
-const panel = document.getElementById('privacy-panel');
-
-if(agree === 'yes') {
-  document.body.removeChild(panel);
-} else {
-  document.getElementById('agreebtn').onclick = function() {
-    Cookie.set('cookie-agree', 'yes', {expire: 7});
-    document.body.removeChild(panel);
-  };
-}
-
-document.getElementById('testbtn').onclick = function() {
-  Cookies.remove('cookie-agree');
-};
-

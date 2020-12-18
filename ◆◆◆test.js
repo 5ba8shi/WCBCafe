e71e2ef96eaf4975d4e08bc7708dfa4a6ed12070
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-12-19 07:18:38 +0900

    Update ◆◆◆test.js

---
+++ ◆◆◆test.js
@@ -0,0 +1,5 @@
+const jsbook = {title:'Javascript入門', price:2500, stock:3};
+
+document.getElementById('title').textContent = jsbook.title;
+document.getElementById('price').textContent = jsbook.price;
+document.getElementById('stock').textContent = jsbook.stock;

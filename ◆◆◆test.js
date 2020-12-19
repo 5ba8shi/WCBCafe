31b78b05b63366f81e18f23f8638a65d4352724e
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-12-19 16:04:42 +0900

    Update ◆◆◆test.js

--- ◆◆◆test.js
+++ ◆◆◆test.js
@@ -9,3 +9,27 @@ function point(num, digit) {
 
 
 document.getElementById('output').textContent = point(Math.PI, 2);
+
+
+const video = document.getElementById('video');
+const play = document.getElementById('play');
+const stop = document.getElementById('stop');
+const progress = document.getElementById('progress');
+const timestamp = document.getElementById('timestamp');
+
+function toggleVideoStatus() {
+  if(video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+function updatePlayIcon() {
+  if(video.paused) {
+    play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
+  } else {
+    play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
+  }
+}
+

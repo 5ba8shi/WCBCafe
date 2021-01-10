208c9473471494f21eb6367fefb362aae65bd523
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2021-01-11 08:45:49 +0900

    Update ◆◆◆test.js

--- ◆◆◆test.js
+++ ◆◆◆test.js
@@ -1,4 +1,42 @@
 window.addEventListener('load', function () {
 
-  const year_box = 
-})
+  const year_box = document.getElementById("year")
+  const month_box = document.getElementById("month")
+  const day_box = document.getElementById("day")
+
+  // 計算ボタン
+  const calc = document.getElementById("calc");
+  // 計算結果
+  const result = document.getElementById("result");
+
+  // エラー要素
+  const errorElement1 = document.getElementById('error1');
+  const errorElement2 = document.getElementById('error2');
+  const errorElement3 = document.getElementById('error3');
+  const errorElement4 = document.getElementById('error4');
+  const errorElement5 = document.getElementById('error5');
+
+  // 正規表現
+  const regexp = /^[0]+[0-9]/;
+  const regexp1 = /[0-9]/;
+
+
+  year_box.addEventListener('blur', function() {
+    const year = document.getElementById("year").value;
+
+    if(year !== '') {
+      
+      if(!year.match(regexp1) || year.match(regexp) || year.length > 4){
+        errorElement1.innerText = "値が不正です";
+
+      }else if(year.match(regexp1) && !year.match(regexp)) {
+        errorElement1.innerText = "";
+      };
+    } else if(errorElement1.innerText !== "" && year === ""){
+      errorElement1.innerText = "";
+      
+    }
+  });
+
+
+});

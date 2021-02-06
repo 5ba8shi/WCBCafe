74596b998f0151535722b7f6958e06f7b2913300
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2021-02-06 10:32:33 +0900

    Update ◆◆◆test.js

--- ◆◆◆test.js
+++ ◆◆◆test.js
@@ -1,7 +1,14 @@
-function total(price) {
-  const tax = 0.1;
-  return price + price * tax;
-}
+window.addEventListener("load", function(){
+  let tabs = document.getElementsByClassName("menu_item");
 
-console.log('コーヒーメーカーの値段は' + total(8000) + '円(税込み)です。');
-document.getElementById('output').textContent = 'コーヒーメーカの値段は' + total(8000) + '円(税込)です。';
+  tabsAry = Array.prototype.slice.call(tabs);
+
+  function tabSwitch(){
+  }
+
+  tabsAry.forEach(function(value) {
+    value.addEventListener("click", tabSwitch)
+  })
+    
+  });
+})
